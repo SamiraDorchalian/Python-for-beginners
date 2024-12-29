34719ceab9ef0bca4641ef209f684dff97afe7c7
samira dorchalian <samiradorchalain1998@gmail.com>
2024-12-29 15:08:11 +0330

    create get_user_password_length

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -7,6 +7,24 @@ settings = {
     'length': 8
 }
 
+def get_user_password_length(option, default, pw_min_len = 4, pw_max_length = 30):
+    while True:
+        user_input = input(f'Enter password length. (Default is {default}) (enter: default): ')
+        
+        if user_input == '':
+            return default
+
+        if user_input.isdigit():
+            user_password_length = int(user_input)
+            if pw_min_len <= user_password_length <= pw_max_length:
+                return int(user_input)
+            print('Invalid input.')
+            print(f'Password length should be between {pw_min_len} and {pw_max_length}.')
+        else:
+            print('Invalid input. You should enter a number.')
+        
+        print('Please try again.')
+
 def get_yes_or_no_for_settings(option, default):
     while True:
         user_input = input(f'Include {option}? (Default is {default}) (y: yes, n, no, enter : default ): ')
@@ -25,7 +43,9 @@ def get_settings_from_user(settings):
         if option != 'length':
             user_choice= get_yes_or_no_for_settings(option, default)
             settings[option] = user_choice
+        else:
+            user_password_length = get_user_password_length(option, default)
+            settings[option] = user_password_length
             
-
 get_settings_from_user(settings)
 print(settings)
