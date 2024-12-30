363a876d5776ba39808390b85e3ca8746c73ba4a
samira dorchalian <samiradorchalain1998@gmail.com>
2024-12-30 15:26:39 +0330

    Just practice

---
+++ test.py
@@ -0,0 +1,44 @@
+import os
+os.system('cls')
+
+# class Car:
+#     def __init__(self, color, model):
+#         self.color = color
+#         self.model = model
+#     def print_details(self):
+#         return f'Color : {self.color} , Model : {self.model}'
+    
+# car1 = Car('blue' , 'BMW')
+# car2 = Car('yellow', 'ferary')
+# car3 = Car('red', 'Samand')
+
+# print( car1 , car2, car3)
+# print(car1.print_details())
+# print(car2.print_details())
+# print(car3.print_details())
+
+
+
+
+# class House:
+
+#     country = 'iren'
+#     x = 123
+
+#     def __init__(self, color, num_of_room, a):
+#         self.color = color
+#         self.num_of_room = num_of_room
+#         self.a = a
+#     def house_description(self):
+#         return (self.color, self.num_of_room, self.a)
+
+# house1 = House('blue', 4 , 100)
+
+# print(house1)
+# print(house1.country)
+# house1.country = 'USA'
+# print(house1.country)
+# del house1.country
+# print(house1.country)
+
+
