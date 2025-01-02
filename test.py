9f9e748a059831443a93480d2e521fc2094414e2
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-02 14:45:38 +0330

    installation and use of ready-made packages & and how to use the module

--- test.py
+++ test.py
@@ -174,3 +174,24 @@ os.system('cls')
 # b = B()
 # b.value = 4
 # a == b
+
+#-----------------------------------------------#
+### Installation and use of ready-made packages
+
+
+import sys
+
+from termcolor import colored, cprint
+
+text = colored("Hello, World!", "red", attrs=[ "blink"])
+print(text)
+cprint("Hello, World!", "green", "on_red")
+
+print_red_on_cyan = lambda x: cprint(x, "red", "on_cyan")
+print_red_on_cyan("Hello, World!")
+print_red_on_cyan("Hello, Universe!")
+
+for i in range(10):
+    cprint(i, "magenta", end=" ")
+
+cprint("Attention!", "yellow", attrs=["underline"], file=sys.stderr)
