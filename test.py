8ca515521f5a8a5aae6b87fbfe3a1770ca368c46
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-03 13:01:47 +0330

    Installation of ready-made packages & installation of virtual environment and how to use virtual environment. Creating the requirement.txt file, and transferring the packages installed in that file.

--- test.py
+++ test.py
@@ -179,19 +179,21 @@ os.system('cls')
 ### Installation and use of ready-made packages
 
 
-import sys
+# import sys
 
-from termcolor import colored, cprint
+# from termcolor import colored, cprint
 
-text = colored("Hello, World!", "red", attrs=[ "blink"])
-print(text)
-cprint("Hello, World!", "green", "on_red")
+# text = colored("Hello, World!", "red", attrs=[ "blink"])
+# print(text)
+# cprint("Hello, World!", "green", "on_red")
 
-print_red_on_cyan = lambda x: cprint(x, "red", "on_cyan")
-print_red_on_cyan("Hello, World!")
-print_red_on_cyan("Hello, Universe!")
+# print_red_on_cyan = lambda x: cprint(x, "red", "on_cyan")
+# print_red_on_cyan("Hello, World!")
+# print_red_on_cyan("Hello, Universe!")
 
-for i in range(10):
-    cprint(i, "magenta", end=" ")
+# for i in range(10):
+#     cprint(i, "magenta", end=" ")
 
-cprint("Attention!", "yellow", attrs=["underline"], file=sys.stderr)
+# cprint("Attention!", "yellow", attrs=["underline"], file=sys.stderr)
+
+#-----------------------------------------------#
