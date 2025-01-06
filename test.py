0cc92f06cdbbfa4acdd038052868b1210508394d
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-06 18:08:22 +0330

    use columnspan from grid

--- test.py
+++ test.py
@@ -503,6 +503,12 @@ import tkinter as tk
 
 window = tk.Tk()
 
+label_title = tk.Label(
+    window,
+    text='Enter your data',
+)
+label_title.grid(row=0, column=0, columnspan=2)
+
 label_input_name = tk.Label(
     window,
     text='First Name: ',
@@ -510,8 +516,8 @@ label_input_name = tk.Label(
 entry_name = tk.Entry(
     window,
 )
-label_input_name.grid(row=0,column=0)
-entry_name.grid(row=0, column=1)
+label_input_name.grid(row=1,column=0)
+entry_name.grid(row=1, column=1)
 
 
 label_input_last_name = tk.Label(
@@ -521,8 +527,8 @@ label_input_last_name = tk.Label(
 entry_last_name = tk.Entry(
     window,
 )
-label_input_last_name.grid(row=1,column=0)
-entry_last_name.grid(row=1, column=1)
+label_input_last_name.grid(row=2,column=0)
+entry_last_name.grid(row=2, column=1)
 
 label_age = tk.Label(
     window,
@@ -531,11 +537,13 @@ label_age = tk.Label(
 entry_age = tk.Entry(
     window,
 )
-label_age.grid(row=2,column=0)
-entry_age.grid(row=2,column=1)
+label_age.grid(row=3,column=0)
+entry_age.grid(row=3,column=1)
 
-
-
-
+submit_button = tk.Button(
+    window,
+    text='Submit',
+)
+submit_button.grid(row=4,column=0, columnspan=2)
 
 window.mainloop()
