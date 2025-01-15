135015f84f84dc87077b8fbcffefe262bb626024
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-15 13:38:56 +0330

    create function check_spelling

--- Spelling Checker App/Spelling Checker App.py
+++ Spelling Checker App/Spelling Checker App.py
@@ -8,6 +8,23 @@ root=Tk()
 root.title('Spelling checker')
 root.geometry('700x400') #Width & Height
 root.config(background='#dae6f6')
+#function
+def check_spelling():
+    word=entry_text.get()
+    a=TextBlob(word)
+    right=str(a.correct())
+
+    cs=Label(
+        master=root,
+        text='Correct text is :',
+        font=('poppins',20),
+        bg='#dae6f6',
+        fg='#364971'
+    )
+    cs.place(x=100,y=250)
+    spell.config(
+        text=right
+    )
 
 #Label Text
 heading=Label(
@@ -36,6 +53,7 @@ button=Button(
     font=('arial',20,'bold'),
     fg='white',
     bg='red',
+    command=check_spelling
 )
 button.pack()
 #Label spell
