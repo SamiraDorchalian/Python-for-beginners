da0d42eab57f5ac018cc818d7dce656c5974ad38
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-20 17:05:29 +0330

    completed function browseFile

--- pdf_viewer_app_using_python/pdf_viewer_app_using_python.py
+++ pdf_viewer_app_using_python/pdf_viewer_app_using_python.py
@@ -2,8 +2,10 @@
 
 from tkinter import *
 from tkinter import filedialog
-from tkPDFViewer import tkPDFViewer as pdf
+# from tkPDFViewer import tkPDFViewer as pdf
+import pypdf as pdf
 import os
+
 
 root=Tk()
 root.geometry('630x700+400+100')
@@ -11,15 +13,18 @@ root.title('PDF viewer')
 root.configure(bg='white')
 
 def browseFile():
-    filename=filedialog.askopenfilename(
-        initialdir=os.getcwd(),
-        title='select pdf file',
-        filetypes=(
-            ('PDF File','.pdf'),
-            ('PDF File','.PDF'),
-            ('All file','.txt')
-        )
-    )
+    filename=filedialog.askopenfilename(initialdir=os.getcwd(),
+                                        title='select pdf file',
+                                        filetypes=(('PDF File','.pdf'),
+                                                    ('PDF File','.PDF'),
+                                                    ('All file','.txt')))
+    # v1=pdf.ShowPdf()
+    # v2=v1.pdf_view(root,pdf_location=open(filename,'r'),width=77,height=100,)
+    # v2.pack(pady=(0,0))
+    reader = pdf.PdfReader(filename)
+    print(len(reader.pages))
+    print(reader.pages[0].extract_text())
+
 
 Button(
     master=root,
