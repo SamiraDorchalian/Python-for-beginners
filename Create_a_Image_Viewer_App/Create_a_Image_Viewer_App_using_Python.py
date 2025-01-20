a929eccec935fb56d0b057eb8dc2b6efe9d348c4
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-20 13:08:54 +0330

    create showimage function

--- Create_a_Image_Viewer_App/Create_a_Image_Viewer_App_using_Python.py
+++ Create_a_Image_Viewer_App/Create_a_Image_Viewer_App_using_Python.py
@@ -6,6 +6,21 @@ import tkinter as tk
 from PIL import Image,ImageTk
 import os
 
+def showimage():
+    filename=filedialog.askopenfilename(
+        initialdir=os.getcwd(),
+        title='Select image file', 
+        filetypes=(
+            ('JPG File','*.jpg'),
+            ('PNG file','*.png'),
+            ('All file ','how are you .txt')
+        )
+    )
+    img=Image.open(filename)
+    img=ImageTk.PhotoImage(img)
+    lbl.configure(image=img)
+    lbl.image=img
+
 root=Tk()
 
 fram=Frame(root)
@@ -14,7 +29,7 @@ fram.pack(side=BOTTOM,padx=15,pady=15)
 lbl=Label(root)
 lbl.pack()
 
-btn1=Button(fram,text='Select Image')
+btn1=Button(fram,text='Select Image',command=showimage)
 btn1.pack(side=tk.LEFT)
 
 btn2=Button(fram,text='Exit',command=lambda:exit())
