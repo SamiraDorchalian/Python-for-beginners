001bacc8e268ff05f824af915b718e9fd63e9269
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-22 14:04:46 +0330

    completed function register create file .txt and push there our information and then deleted Previous information in entry form

--- Registration_Form/Registration_Form.py
+++ Registration_Form/Registration_Form.py
@@ -7,7 +7,24 @@ root.resizable(False,False)
 
 #functin
 def register():
-    print('registered')
+    name_info=nameValue.get()
+    phone_info=phoneValue.get()
+    gender_info=genderValue.get()
+    email_info=emailValue.get()
+
+    file=open(name_info + '.txt','w')
+    file.write(name_info+'\n')
+    file.write(phone_info+'\n')
+    file.write(gender_info+'\n')
+    file.write(email_info+'\n')
+    file.close()
+
+    nameEntry.delete(0,END)
+    phoneEntry.delete(0,END)
+    genderEntry.delete(0,END)
+    emailEntry.delete(0,END)
+
+    Label(text='Registration success',fg='green', font=('calibri', 11)).place(x=250,y=430)
 
 #labels
 Label(
