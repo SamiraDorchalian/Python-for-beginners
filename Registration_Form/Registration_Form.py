c299769c3b31f5c835ea05090ed4f0886dffec1b
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-22 13:42:24 +0330

    create from registration label & entry

---
+++ Registration_Form/Registration_Form.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+root=Tk()
+root.title('Refisteation')
+root.geometry('600x470')
+root.resizable(False,False)
+
+Label(
+    root,
+    text='Python Registration Form',
+    font='arial 25',
+).pack(pady=50)
+
+Label(text='Name',font=23).place(x=100,y=150)
+Label(text='Phone',font=23).place(x=100,y=200)
+Label(text='Gender',font=23).place(x=100,y=250)
+Label(text='Email',font=23).place(x=100,y=300)
+
+#entry
+nameValue=StringVar()
+phoneValue=StringVar()
+genderValue=StringVar()
+emailValue=StringVar()
+
+nameEntry=Entry(root,textvariable=nameValue,width=30,font=20, bd=2)
+phoneEntry=Entry(root,textvariable=phoneValue,width=30,font=20, bd=2)
+genderEntry=Entry(root,textvariable=genderValue,width=30,font=20, bd=2)
+emailEntry=Entry(root,textvariable=emailValue,width=30,font=20, bd=2)
+
+nameEntry.place(x=200,y=150)
+phoneEntry.place(x=200,y=200)
+genderEntry.place(x=200,y=250)
+emailEntry.place(x=200,y=300)
+root.mainloop()
